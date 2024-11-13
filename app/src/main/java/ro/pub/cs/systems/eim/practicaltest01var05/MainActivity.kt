7b9081283eb180a52e0e07c496cdffd24fb473ed
Andi Ciupitu <andi_mihai.ciupitu@stud.acs.upb.ro>
2024-11-13 08:22:48 +0200

    TASK A REZOLVAT

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/MainActivity.kt
@@ -0,0 +1,38 @@
+package ro.pub.cs.systems.eim.practicaltest01var05
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ro.pub.cs.systems.eim.practicaltest01var05.ui.theme.PracticalTest01Var05Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_practical_test01_var05_main)
+
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PracticalTest01Var05Theme {
+        Greeting("Android")
+    }
+}
